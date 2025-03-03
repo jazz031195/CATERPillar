a92c25e0f0fa8522ccea35d886cfc6f15ecab33b
jazz031195 <jasmine.nguyenduc@gmail.com>
2025-03-03 14:00:21 +0100

    update

--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -l
-chmod u+x compiler.sh
-./compiler.sh
-cd /home/localadmin/Documents/CATERPillar/src/
-chmod u+x Growth-Animation.exe
-./Growth-Animation.exe "/home/localadmin/Documents/CATERPillar/args.conf"
